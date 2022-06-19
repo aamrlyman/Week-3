67520a2bb6d9c69ac9262363e22e8e181343e853
aamrlyman <aamrlyman@gmail.com>
2022-06-19 09:49:11 -0700

    got compressor working and deleted failed attempts

--- Problem Solving WKS/problem_solving_one.py
+++ Problem Solving WKS/problem_solving_one.py
@@ -1,64 +1,34 @@
-# def reverse_string(string):
-#     reversed_string = ''
-#     for index in range(len(string)-1, -1, -1):
-#         reversed_string += string[index]
-#     return reversed_string
+def reverse_string(string):
+    reversed_string = ''
+    for index in range(len(string)-1, -1, -1):
+        reversed_string += string[index]
+    return reversed_string
 
-# print(reverse_string('Hello'))
+print(reverse_string('Hello'))
 
 
-# def capitalize_words_in_string(string):
-#     if "  " in string:
-#             print('invalid entry string contains 2 consecutive spaces')
-#     new_str = ''
-#     temp = 0
-#     for index in range(len(string)):
-#         if index == 0:
-#             new_str += string[index].upper()
-#         elif index == temp:
-#             continue
-#         elif string[index] == " ":
-#             new_str += string[index] + string[index + 1].upper()
-#             temp = index + 1
-#         else:
-#             new_str += string[index]
+def capitalize_words_in_string(string):
+    if "  " in string:
+            print('invalid entry string contains 2 consecutive spaces')
+    new_str = ''
+    temp = 0
+    for index in range(len(string)):
+        if index == 0:
+            new_str += string[index].upper()
+        elif index == temp:
+            continue
+        elif string[index] == " ":
+            new_str += string[index] + string[index + 1].upper()
+            temp = index + 1
+        else:
+            new_str += string[index]
         
-#     return new_str
+    return new_str
 
-# print(capitalize_words_in_string('hello'))
-# print(capitalize_words_in_string('hello goodbye'))
-# print(capitalize_words_in_string('hello goodbye thanks'))
-# print(capitalize_words_in_string('hello goodbye  thanks'))
-
-# def compress_string(string):
-#     temp1 = ''
-#     temp2= ''
-#     new_string = ''
-#     counter = 0
-#     for char in string:
-#         #counter += 1
-#         if temp1 == char:
-#             continue
-#         temp1 = char
-
-#         for index in range(len(string)):
-#             if string[index] == temp2:
-#                 continue
-#             if string[index] == temp1 and index == (len(string) - 1):
-#                 counter += 1
-#                 new_string+= str(counter) + temp1
-#                 return new_string
-#             elif string[index] == temp1:
-#                 counter += 1
-#             elif string[index] != temp1 or string[index] != temp2:
-#                 new_string += str(counter) + temp1
-#                 counter = 0
-#                 temp2 = temp1
-#                 break        
-    
-#     return new_string
-
-
+print(capitalize_words_in_string('hello'))
+print(capitalize_words_in_string('hello goodbye'))
+print(capitalize_words_in_string('hello goodbye thanks'))
+print(capitalize_words_in_string('hello goodbye  thanks'))
 
 
 def compress_string(string):
@@ -93,3 +63,12 @@ print(compress_string('aaa'))
 print(compress_string('aaabbaa'))
 print(compress_string('aaabbcc'))
 print(compress_string("aaabbbbbccccaacccbbbaaabbbaaa"), "3a5b4c2a3c3b3a3b3a")
+
+def is_palindrome(string):
+    reversed_string = ''
+    for index in range(len(string) -1, -1, -1):
+        reversed_string += string[index]
+    
+    return string == reversed_string
+
+print(is_palindrome('madam'))
